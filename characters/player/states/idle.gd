811549b0223002c6eb23b7b64cfcc937c57b5ee4
ZalewskiPiotr <1piotrzalewski@gmail.com>
2024-10-29 21:17:22 +0100

    Dodano obsługę ruchu góra-dół

--- characters/player/states/idle.gd
+++ characters/player/states/idle.gd
@@ -10,12 +10,16 @@ func physics_update(_delta: float) -> void:
 	player.move_and_slide()
 	
 	# Wciśnięcie jednocześnie klawiszy ruchu powoduje pozostanie w stanie bezczynności
-	if Input.is_action_pressed("move_left") and (Input.is_action_pressed("move_right")):
+	if (Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right")) or \
+			(Input.is_action_pressed("move_up") and Input.is_action_pressed("move_down")):
 		return
 	
 	# Zmiana stanu
-	if Input.is_action_pressed("run") and (Input.is_action_pressed("move_left") or 
-			Input.is_action_pressed("move_right")):
+	if Input.is_action_pressed("run") and (Input.is_action_pressed("move_left") or \
+			Input.is_action_pressed("move_right") or \
+			Input.is_action_pressed("move_up") or \
+			Input.is_action_pressed("move_down")):
 		finished.emit(RUN) # Bieganie
-	elif Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right") or \
+			Input.is_action_pressed("move_up") or Input.is_action_pressed("move_down"):
 		finished.emit(WALK) # Chodzenie
