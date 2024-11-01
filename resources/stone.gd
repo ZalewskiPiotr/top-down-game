995ffed159aee4d358458d4af4671db68d46d51a
ZalewskiPiotr <1piotrzalewski@gmail.com>
2024-11-01 21:35:18 +0100

    komentarze

--- resources/stone.gd
+++ resources/stone.gd
@@ -5,6 +5,6 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		# TODO: trzeba dodać obiekt do inwentory
+		# TODO: trzeba dodać obiekt do inwentory. Tu chyba wyemitujemy jaki sygnał z ilością oraz nazwą zasobów
 		print("pick up")
 		self.queue_free()
